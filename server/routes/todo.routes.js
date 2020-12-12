83ee372411ea05a84eeb608899f6313c87a25348
LayneHansen <layne.d.hansen@gmail.com>
2020-12-11 21:25:56 -0700

    routes file, models

--- server/routes/todo.routes.js
+++ server/routes/todo.routes.js
@@ -1,14 +1,57 @@
 const router = require('express').Router();
+const { Todo } = require('../models');
 
 router  
     .route('/')
     .get((req, res) => {
+        Todo
+        .find({})
+        .then(data => {
+            console.log({'TODO GET'})
+            console.log({ data })
+            res.json({ success: true, data })
+        })
+        .catch(err => {
+            console.log({ err });
+            res.json({ success: false });
+        });
         console.log('hit the get route in /api/todos');
         res.json({ success: true });
     })
     .post((req, res) => {
         console.log({ reqBody: req.body });
         res.json({ success: true });
+        
+        Todo
+        .create({
+            text: req.body.text
+        })
+        .then(data => {
+            console.log({ data })
+            res.json({ success: true, data })
+        })
+        .catch(err => {
+            console.log({ err });
+            res.json({ success: false });
+        });
     });
+
+    router  
+        .route('/:id')
+        .delete((req, res) => {
+            console.log(req.params);
+            res.json({ success: true });
+
+            Todo
+                .findByIdAndDelete(req.params.id)
+                .then(data => {
+                    console.log({ data })
+                    res.json({ success: true });
+                })
+                .catch(err => {
+                    console.log({ err })
+                    res.json({ success: false });
+                });
+        });
 
     module.exports = router;
